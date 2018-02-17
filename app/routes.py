b8e3eb596e17370e6a78223866c46dbb856c03b6
sivakrishna7 <sivakrishna@uky.edu>
2018-02-17 18:41:41 -0500

    Added templates(v0.2)

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,20 @@
+from flask import render_template
 # app - package, app - object of Flask class instance
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Om Namah Shivaya'
+    user = {'username': 'visweswaraya'}
+    posts = [
+        {
+            'author': {'username': 'Mahadevaya'},
+            'body': 'Om Mahadevaya Namaha'
+        },
+        {
+            'author': {'username': 'Tryambakaya'},
+            'body': 'Om Tryambakaya Namaha'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
